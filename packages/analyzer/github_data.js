7dcf58b7376bbb32ac969d340c1d8368e6d991fe
Andreas Gabriel <gandev.de@gmail.com>
2014-10-17 01:09:18 +0200

    add github auth

--- packages/analyzer/github_data.js
+++ packages/analyzer/github_data.js
@@ -4,11 +4,14 @@ Analyzer.readFileGithub = function(path) {
   var resp = HTTP.get(githubApi + path, {
     headers: {
       'User-Agent': 'gandev'
-    }
+    },
+    auth: '65ee63229a1a28c26e3f1b3f422e1a9f219299a7:x-oauth-basic'
   });
 
   if (resp.data && resp.data.type === "file") {
     var fileContent = new Buffer(resp.data.content, 'base64').toString('binary');
+
+    console.log(path);
 
     return fileContent;
   } else {
@@ -20,7 +23,8 @@ Analyzer.readDirGithub = function(path) {
   var resp = HTTP.get(githubApi + path, {
     headers: {
       'User-Agent': 'gandev'
-    }
+    },
+    auth: '65ee63229a1a28c26e3f1b3f422e1a9f219299a7:x-oauth-basic'
   });
 
   if (resp.data && resp.data instanceof Array) {
--- packages_view.js
+++ packages_view.js
@@ -3,14 +3,13 @@ if (Meteor.isServer) {
     'analyzePackages': function() {
       console.time("analyzePackages");
 
-      Packages.remove({});
-
       //var analyzer = new Analyzer('/Users/ares/dev/js/meteor/packages');
       //var analyzer = new Analyzer('/home/ag/dev/meteor/packages');
-
       var analyzer = new Analyzer("/repos/meteor/meteor/contents/packages", true);
 
       var packages = analyzer.getPackages();
+
+      Packages.remove({});
 
       _.each(packages, function(pkg) {
         Packages.insert(pkg);
