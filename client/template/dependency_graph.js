185eecd97935d55621161acaf5be15e902b56332
Andreas Gabriel <gandev.de@gmail.com>
2014-10-24 13:31:25 +0200

    get rid of force layout and use dendrogramm as dependency graph layout

--- client/template/dependency_graph.js
+++ client/template/dependency_graph.js
@@ -1,209 +1,73 @@
-ForceVariants = {};
+var width;
 
-Session.setDefault("FORCE", "renderForce1");
+var HEIGHT = 300;
+var height;
 
-ForceVariants.renderForce1 = function(svg, force) {
-  force.linkDistance(200)
-    .charge(-300)
-    .start();
+var renderDependencyGraph = function(svg, root, idx) {
+  var cluster = d3.layout.cluster()
+    .size([HEIGHT, width - 400]);
 
-  // build the arrow.
-  svg.append("svg:defs").selectAll("marker")
-    .data(["end"]) // Different link/path types can be defined here
-    .enter().append("svg:marker") // This section adds in the arrows
-    .attr("id", String)
-    .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
-    .attr("refY", -1.5)
-    .attr("markerWidth", 6)
-    .attr("markerHeight", 6)
-    .attr("orient", "auto")
-    .append("svg:path")
-    .attr("d", "M0,-5L10,0L0,5");
-
-  // add the links and the arrows
-  var path = svg.append("svg:g").selectAll("path")
-    .data(force.links())
-    .enter().append("svg:path")
-    .attr("class", "link")
-    .attr("marker-end", "url(#end)");
-
-  // define the nodes
-  var node = svg.selectAll(".node")
-    .data(force.nodes())
-    .enter().append("g")
-    .attr("class", "node")
-    .call(force.drag);
-
-  // add the nodes
-  node.append("circle")
-    .attr("r", function(pkgNode) {
-      var radius = 5;
-      if (_.contains(packagesFilter.get(), pkgNode.name)) {
-        radius = 10;
-      }
-      return radius;
+  var diagonal = d3.svg.diagonal()
+    .projection(function(d) {
+      return [d.y, d.x];
     });
 
-  // add the text
-  node.append("text")
-    .attr("x", 12)
-    .attr("dy", ".35em")
-    .text(function(d) {
-      return d.name;
-    });
+  svg = svg.append("g")
+    .attr("transform", "translate(150," + HEIGHT * idx + ")");
 
-  force.on("tick", function() {
-    path.attr("d", function(d) {
-      // add the curvy lines
-      var dx = d.target.x - d.source.x,
-        dy = d.target.y - d.source.y,
-        dr = Math.sqrt(dx * dx + dy * dy);
-      return "M" +
-        d.source.x + "," +
-        d.source.y + "A" +
-        dr + "," + dr + " 0 0,1 " +
-        d.target.x + "," +
-        d.target.y;
-    });
-
-    node.attr("transform", function(d) {
-      return "translate(" + d.x + "," + d.y + ")";
-    });
-  });
-};
-
-ForceVariants.renderForce2 = function(svg, force) {
-  var color = d3.scale.category20();
-
-  force.charge(-120)
-    .linkDistance(30)
-    .start();
+  var nodes = cluster.nodes(root),
+    links = cluster.links(nodes);
 
   var link = svg.selectAll(".link")
-    .data(force.links())
-    .enter().append("line")
+    .data(links)
+    .enter().append("path")
     .attr("class", "link")
-    .style("stroke-width", function(d) {
-      return Math.sqrt(d.value);
-    });
+    .attr("d", diagonal);
 
-  // var node = svg.selectAll(".node")
-  //   .data(force.nodes())
-  //   .enter().append("circle")
-  //   .attr("class", "node")
-  //   .attr("r", 5)
-  //   .style("fill", function(d) {
-  //     return color(d.group);
-  //   })
-  //   .call(force.drag);
-  //
-  // node.append("title")
-  //   .text(function(d) {
-  //     return d.name;
-  //   });
-
-  // define the nodes
   var node = svg.selectAll(".node")
-    .data(force.nodes())
+    .data(nodes)
     .enter().append("g")
     .attr("class", "node")
-    .call(force.drag);
-
-  // add the nodes
-  node.append("circle")
-    .attr("r", function(pkgNode) {
-      var radius = 5;
-      if (_.contains(packagesFilter.get(), pkgNode.name)) {
-        radius = 10;
-      }
-      return radius;
+    .attr("transform", function(d) {
+      return "translate(" + d.y + "," + d.x + ")";
     });
 
-  // add the text
+  node.append("circle")
+    .attr("r", 4.5);
+
   node.append("text")
-    .attr("x", 12)
-    .attr("dy", ".35em")
+    .attr("dx", function(d) {
+      return d.children ? -8 : 8;
+    })
+    .attr("dy", 3)
+    .style("text-anchor", function(d) {
+      return d.children ? "end" : "start";
+    })
     .text(function(d) {
       return d.name;
     });
-
-  force.on("tick", function() {
-    link.attr("x1", function(d) {
-        return d.source.x;
-      })
-      .attr("y1", function(d) {
-        return d.source.y;
-      })
-      .attr("x2", function(d) {
-        return d.target.x;
-      })
-      .attr("y2", function(d) {
-        return d.target.y;
-      });
-
-
-
-    node.attr("transform", function(d) {
-      return "translate(" + d.x + "," + d.y + ")";
-    });
-  });
 };
 
 var renderAutorun;
 
 Template.dependency_graph.rendered = function() {
+  var adjustWidth = _.debounce(function() {
+    Session.set("DOCUMENT_WIDTH", $(window).width());
+  }, 200);
+
+  adjustWidth();
+
+  $(window).resize(function(arg) {
+    adjustWidth();
+  });
+
   renderAutorun = Tracker.autorun(function() {
+    var packages = getSelectedPackages().fetch();
+
     console.log("rerender graph");
 
-    var allPackages = Packages.find().fetch();
-
-    var packages = {};
-    _.each(allPackages, function(pkg) {
-      packages[pkg.name] = pkg;
-    });
-
-    var packagesToWatch = getSelectedPackages().fetch();
-
-    var links = [];
-
-    var nodes = {};
-    _.each(packagesToWatch, function(pkg) {
-      nodes[pkg.name] = packages[pkg.name];
-    });
-
-    _.each(packagesToWatch, function(pkg) {
-      _.each(pkg.usedInPackages, function(name) {
-        if (packages[name]) {
-          if (!nodes[name]) {
-            nodes[name] = packages[name];
-          }
-
-          links.push({
-            source: packages[pkg.name],
-            target: packages[name],
-            value: 1.0
-          });
-        }
-      });
-
-      _.each(pkg.uses, function(use) {
-        if (packages[use.name]) {
-          if (!nodes[use.name]) {
-            nodes[use.name] = packages[use.name];
-          }
-
-          links.push({
-            source: packages[use.name],
-            target: packages[pkg.name],
-            value: 1.0
-          });
-        }
-      });
-    });
-
-    var width = 900,
-      height = 600;
+    width = Session.get("DOCUMENT_WIDTH");
+    height = HEIGHT * packages.length;
 
     var svg = d3.select("#dependency-graph")
       .attr("width", width)
@@ -211,12 +75,27 @@ Template.dependency_graph.rendered = function() {
 
     svg.selectAll("*").remove();
 
-    var force = d3.layout.force()
-      .nodes(d3.values(nodes))
-      .links(links)
-      .size([width, height]);
+    _.each(packages, function(pkg, idx) {
+      var root = {
+        name: pkg.name,
+        children: []
+      };
 
-    ForceVariants[Session.get("FORCE")](svg, force);
+      _.each(pkg.usedInPackages, function(name) {
+        root.children.push({
+          name: name,
+          children: []
+        });
+      });
+
+      _.each(pkg.uses, function(use) {
+        if (use.name) {
+          //TODO uses on the left
+        }
+      });
+
+      renderDependencyGraph(svg, root, idx);
+    });
   });
 };
 
