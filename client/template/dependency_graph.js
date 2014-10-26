402449b2b988bf6ec88ca94fb06559f9fe518694
Andreas Gabriel <gandev.de@gmail.com>
2014-10-26 03:21:10 +0100

    another dependency graph version ...

--- client/template/dependency_graph.js
+++ client/template/dependency_graph.js
@@ -1,58 +1,155 @@
 var width;
-
-var HEIGHT = 300;
 var height;
 
-var renderDependencyGraph = function(svg, root, idx) {
-  var cluster = d3.layout.cluster()
-    .size([HEIGHT, width - 400]);
+var createLinksBetweenNodes = function(svg, nodes, map) {
+  var links = [];
+  // For each use, construct a link from the source to target node.
+  _.each(nodes, function(node) {
+    _.each(node.uses || [], function(use) {
+      if (!map[use.name]) return;
 
-  var diagonal = d3.svg.diagonal()
-    .projection(function(d) {
-      return [d.y, d.x];
+      links.push({
+        source: map[node.name],
+        target: map[use.name]
+      });
     });
+  });
+
+  var line = d3.svg.line.radial()
+    .interpolate("bundle")
+    .tension(0.6)
+    .radius(function(d) {
+      return d.y;
+    })
+    .angle(function(d) {
+      return d.x / 180 * Math.PI;
+    });
+
+  var bundle = d3.layout.bundle();
+
+  var paths = svg.append("g").selectAll(".link")
+    .data(bundle(links))
+    .enter()
+    .append("path")
+    .each(function(d) {
+      d.source = d[0];
+      d.target = d[d.length - 1];
+    })
+    .attr("class", "link")
+    .attr("d", line);
+
+  return paths;
+};
+
+var renderDependencyGraph = function(svg, packages) {
+  var root = {
+    name: "",
+    children: _.filter(packages, function(pkg) {
+      return pkg.uses.length > 0 || pkg.usedInPackages && pkg.usedInPackages.length > 0;
+    })
+  };
+
+  var diameter = width;
+  var radius = diameter / 2;
+  var innerRadius = radius - 180;
 
   svg = svg.append("g")
-    .attr("transform", "translate(150," + HEIGHT * idx + ")");
+    .attr("transform", "translate(" + radius + "," + radius + ")");
 
-  var nodes = cluster.nodes(root),
-    links = cluster.links(nodes);
+  var cluster = d3.layout.cluster()
+    .size([360, innerRadius]);
 
-  var link = svg.selectAll(".link")
-    .data(links)
-    .enter().append("path")
-    .attr("class", "link")
-    .attr("d", diagonal);
+  var node = svg.append("g").selectAll(".node");
 
-  var node = svg.selectAll(".node")
-    .data(nodes)
-    .enter().append("g")
-    .attr("class", "node")
-    .attr("transform", function(d) {
-      return "translate(" + d.y + "," + d.x + ")";
+  var nodes = cluster.nodes(root);
+  nodes.splice(0, 1); //remove root node
+
+  var map = {};
+  _.each(nodes, function(node) {
+    map[node.name] = node;
+  });
+
+  var links = createLinksBetweenNodes(svg, nodes, map);
+
+  var selectNodes = function(d, i, n, fromExtern) {
+    if (packagesFilter.get().length > 0 && !fromExtern) return;
+
+    node.each(function(n) {
+      n.target = false;
+      n.source = false;
     });
 
-  node.append("circle")
-    .attr("r", 4.5);
+    links.classed("link--target", function(l) {
+        if (fromExtern && _.contains(d, l.target) || l.target === d) {
+          l.source.source = true;
+          return true;
+        }
+      })
+      .classed("link--source", function(l) {
+        if (fromExtern && _.contains(d, l.source) || l.source === d) {
+          l.target.target = true;
+          return true;
+        }
+      });
 
-  node.append("text")
-    .attr("dx", function(d) {
-      return d.children ? -8 : 8;
+    node.classed("node--target", function(n) {
+        return n.target;
+      })
+      .classed("node--source", function(n) {
+        return n.source;
+      });
+  };
+
+  var clearSelection = function(d) {
+    if (packagesFilter.get().length > 0 && d) return;
+
+    links.classed("link--target", false)
+      .classed("link--source", false);
+
+    node.classed("node--target", false)
+      .classed("node--source", false)
+      .classed("node-selected", false);
+  };
+
+  node = node.data(nodes)
+    .enter().append("text")
+    .attr("class", "node")
+    .attr("dy", ".31em")
+    .attr("transform", function(d) {
+      return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)");
     })
-    .attr("dy", 3)
     .style("text-anchor", function(d) {
-      return d.children ? "end" : "start";
+      return d.x < 180 ? "start" : "end";
     })
     .text(function(d) {
       return d.name;
+    })
+    .on("mouseover", selectNodes)
+    .on("mouseout", clearSelection);
+
+  Tracker.autorun(function() {
+    var selectedPackages = getSelectedPackages().fetch();
+
+    clearSelection(null, true);
+
+    var selectedNodes = [];
+    _.each(selectedPackages, function(pkg) {
+      selectedNodes.push(map[pkg.name]);
     });
+
+    node.classed("node-selected", function(d) {
+      return _.contains(selectedNodes, d);
+    });
+
+    selectNodes(selectedNodes, 0, 0, true);
+  });
 };
 
 var renderAutorun;
 
 Template.dependency_graph.rendered = function() {
   var adjustWidth = _.debounce(function() {
-    Session.set("DOCUMENT_WIDTH", $(window).width());
+    Session.set("GRAPH_WIDTH", $(".container-fluid").width());
   }, 200);
 
   adjustWidth();
@@ -62,12 +159,12 @@ Template.dependency_graph.rendered = function() {
   });
 
   renderAutorun = Tracker.autorun(function() {
-    var packages = getSelectedPackages().fetch();
+    var packages = Packages.find().fetch();
 
     console.log("rerender graph");
 
-    width = Session.get("DOCUMENT_WIDTH");
-    height = HEIGHT * packages.length;
+    width = Session.get("GRAPH_WIDTH");
+    height = width;
 
     var svg = d3.select("#dependency-graph")
       .attr("width", width)
@@ -75,27 +172,7 @@ Template.dependency_graph.rendered = function() {
 
     svg.selectAll("*").remove();
 
-    _.each(packages, function(pkg, idx) {
-      var root = {
-        name: pkg.name,
-        children: []
-      };
-
-      _.each(pkg.usedInPackages, function(name) {
-        root.children.push({
-          name: name,
-          children: []
-        });
-      });
-
-      _.each(pkg.uses, function(use) {
-        if (use.name) {
-          //TODO uses on the left
-        }
-      });
-
-      renderDependencyGraph(svg, root, idx);
-    });
+    renderDependencyGraph(svg, packages);
   });
 };
 
